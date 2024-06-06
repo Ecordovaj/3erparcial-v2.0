10b04bbfab07972a3676e1874e2f90371833c01e
JoseEmilianoCordovaLima <168248461+Ecordovaj@users.noreply.github.com>
2024-06-06 10:35:54 -0600

    ejercicio1: contraseña 3 numeros
    contraseña 3 números

---
+++ contraseña3numeros.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, c;
+    std::cout << "Ingresa tu contraseña";
+    std::cin >> a >> b >> c;
+
+    if (a==1&&b==2&&c==3){
+        
+    cout<< "esta bien su contraseña";
+    }
+    
+    else {
+        
+        cout << "su contraseña esta mal";
+    }
+    
+
+    return 0;
+}

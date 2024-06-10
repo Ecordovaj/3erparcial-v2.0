df0a619fee4ba7c5b04d1690c2fa28890304aee0
JoseEmilianoCordovaLima <168248461+Ecordovaj@users.noreply.github.com>
2024-06-10 11:00:31 -0600

    ejercicio54.cpp
    ejercicio raiz cuadrada

---
+++ ejercicio54.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    double num;
+  
+    cout << "Ingrese un numero entero ";
+  
+    cin >> num;
+  
+    double raiz = pow(num, 0.5);
+    
+  cout << "La raiz cuadrada de " << num << " es: " << raiz << endl;
+  
+  return 0;
+}
